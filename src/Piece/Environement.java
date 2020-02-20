02676e9e1783c7be74e255a5183ce97edd64e5b2
marjoriePaillet <marjoriepaillet@live.fr>
2020-02-20 17:29:42 +0100

    ajout classe Environement + début IHM

--- src/Piece/Environement.java
+++ src/Piece/Environement.java
@@ -1,5 +1,52 @@
 package Piece;
+import java.util.ArrayList;
+import Piece.Element;
+import Hardware.Robot;
 
 public class Environement {
-
+ 
+	// attributs 
+	protected Robot rob;
+	protected ArrayList<Element> elems;
+	protected int sizeX;
+	protected int sizeY;
+	
+	// constructeur 
+	
+	Environement(int nsizeX, int nsizeY){
+		this.sizeX=nsizeX;
+		this.sizeY=nsizeY;
+	}
+	
+	//méthodes 
+	public void addRobot(Robot r){
+		this.rob = r;
+	}
+	
+	public void addElem(Element e){
+		this.elems.add(e);
+	}
+	
+	public int getSizeX() {
+		return this.sizeX;
+	}
+	
+	public int getSizeY() {
+		return this.sizeY;
+	}
+	
+	public ArrayList<Element> getElems(){
+		return elems;
+	}
+	
+	public void removeElem(Element e) {
+		if (elems.remove(e) == false) {
+			System.out.println("l'élément à supprimer n'existe pas dans la liste");
+		}
+	}
+	
+	public String toString() {
+		return ("l'environement a une taille de" + this.sizeX + "sur" + this.sizeY + "et contient" + this.elems.size()+"elements");
+	}
 }
+
--- src/Simulateur/IHMRoomba.java
+++ src/Simulateur/IHMRoomba.java
@@ -1,7 +1,37 @@
 package Simulateur;
 import java.awt.*;
 import javax.swing.*;
+import Piece.Environement;
+import Piece.Element;
 
 public class IHMRoomba extends JPanel {
-
+	
+	//Attribut
+	private Environement env;
+	
+	//Constructeur 
+	
+	IHMRoomba (Environement nenv){
+		this.env = nenv;
+	}
+	
+	public void paint(Graphics g) {
+		super.paint(g);
+		Graphics2D g2 = (Graphics2D) g;
+		Graphics2D g3 = (Graphics2D) g;
+		g2.setColor(Color.black);
+		g2.fillRect(prev_x,prev_y,40,40);
+		g2.setColor(Color.red);
+		g2.fillOval(x, y, 40, 40);
+		g3.setColor(Color.blue);
+		g3.fillRect(100, 100, 40, 30);
+		g2.dispose();
+		g3.dispose();
+		prev_x=x;
+		prev_y=y;
+	}
+	
+	public void draw(Element e) {
+		
+	}
 }
